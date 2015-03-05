464b0ee715fdc1fcda7208e1ccde72394689d61a
shanickjohnson <nicolejohnson967@gmail.com>
2015-03-04 19:56:43 -0500

    Revised
    Correct Sql that will work in phpmyadmin Also the populated sql file
    contains all computing course

--- SCA.sql
+++ SCA.sql
@@ -1,18 +1,20 @@
 
-create database capstone
+DROP DATABASE IF EXISTS capstone;
+CREATE DATABASE capstone;
+USE capstone;
 
-create table student
-(
+CREATE TABLE student
+(	
+	stid int,
 	fname varchar(25),
 	lname varchar(25),
-	stid int(10),
 	major1 varchar(25),
 	major2 varchar(25),
 	minor1 varchar(25),
 	minor2 varchar(25),
-	level varchar(1),
+	level int,
 	status varchar(10),
-	password varchar(25)
+	password varchar(25),
 
 	primary key (stid)
 
@@ -20,12 +22,12 @@ create table student
 
 create table courses
 	(
-		c_code NOT NULL varchar(8),
-		c_name NOT NULL varchar(50),
-		c_level NOT NULL int,
+		c_code varchar (25),
+		c_name varchar (50),
+		c_level int,
 		c_descrip varchar(500),
 		semester int,
-		creditnum NOT NULL int
+		creditnum int,
 
 
 		primary key (c_code)
@@ -45,9 +47,9 @@ create table prereq
 
 create table degree	/*Need to find a away to account for majors and minors */
 	(
-		id int not null AUTO_INCREMENT,
-		name not null varchar(50),
-		department varchar(50)
+		id int auto_increment NOT NULL,
+		name varchar(50),
+		department varchar(50),
 
 		primary key (id)
 
---
+++ populated courses.sql
@@ -0,0 +1,81 @@
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP1126','INTRODUCTION TO COMPUTING I','1','','','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP1127','INTRODUCTION TO COMPUTING II','1','','','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP1161','OBJECT-OREIENTED PROGRAMMING','1','','','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP1210','MATHEMATICS FOR COMPUTING','1','','','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP1220','COMPUTING AND SOCIETY','1','','','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP2010','PROBABLITY AND STATISTICS','2','','1','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP2120','DIGITAL LOGIC DESIGN','2','','1','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP2130','SYSTEMS PROGRAMMING','2','','','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP2140','SOFTWARE ENGINEERING','2','','1','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP2170','OBJECT TECHNOLOGY','2','','2','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP2190','NET-CENTRIC COMPUTING','2','','1','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP2201','DISCRETE MATHEMATICS FOR COMPUTER SCIENCE','2','','1','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP2211','ANALYSIS OF ALGORITHMS','2','','2','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP2340','COMPUTER SYSTEMS ORGANIZATION','2','','2','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('INFO2100','MATHEMATICS AND STATISTICS FOR IT','2','','2','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('INFO2110','DATA STRUCTURES FOR IT','2','','1','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('INFO2180','DYNAMIC WEB DEVELOPMENT I','2','','1','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP3101','OPERATING SYSTEMS','3','','1','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP3161','DATABASE MANAGEMENT SYSTEMS','3','','2','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP3191','PRINCIPLES OF COMPUTER NETWORKING','3','','1','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP3192','IMPLEMENTATION OF COMPUTER NETWORKS','3','','2','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP3220','PRINCIPLES OF ARTIFICIAL INTELLIGENCE','3','','1','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP3270','USER INTERFACE DESIGN','3','','1','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP3652','LANGUAGE PROCESSORS','3','','','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP3702','THEORY OF COMPUTATION','3','','2','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP3801','REAL-TIME EMBEDDED SYSTEMS','3','','2','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP3901','CAPSTONE PROJECT','3','','2','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP3911','INTERNSHIP IN COMPUTING I','3','','','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('COMP3912','INTERNSHIP IN COMPUTING II','3','','','6');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('INFO3105','COMPUTER SYSTEMS ADMINISTRATION','3','','1','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('INFO3110','INFORMATION SYSTEMS','3','','2','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('INFO3155','INFORMATION ASSURANCE AND SECURITY','3','','2','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('INFO3170','USER INTERFACE DESIGN FOR IT','3','','2','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('INFO3180','DYNAMIC WEB DEVELOPMENT II','3','','2','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('INFO3435','ECOMMERCE','3','','2','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('SWEN3130','SOFTWARE PROJECT MANAGEMENT','3','','1','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('SWEN3145','SOFTWARE MODELING','3','','','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('SWEN3165','SOFTWARE TESTING','3','','2','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('SWEN3185','FORMAL METHODS AND SOFTWARE REALIABLITY','3','','2','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('SWEN3920','CAPSTONE PROJECT (SOFTWARE ENGINEERING)','3','','2','3');
+
+INSERT INTO courses (`c_code`,`c_name`,`c_level`,`c_descrip`,`semester`,`creditnum`) VALUES ('','','','','','');
